bca878afa589fdd8bf1e3ed8269d7102db6720b5
Alan Cortez <78040578+AlanCortez1337@users.noreply.github.com>
2024-02-09 02:43:36 +0000

    my code!

--- vector.hpp
+++ vector.hpp
@@ -11,13 +11,23 @@ public:
     {
         // ⭐TODO: Without using the prebuilt .pop_back() function, finish this function so that it always removes the last element in container_
         // 💡 Dont forget about size_ and capacity_
+        if ( !(size_ < 1)) {
+
+            container_.at(size_-1) = {};
+        size_ = size_ - 1;
+        }
+        // container_.pop_back();
+        
     }
 
     void push_back(T value)
     {
         // ⭐TODO: Without using the prebuilt .push_back() function, finish this function so that it always removes the last element in container_
         // 💡 Dont forget about size_ and capacity_
+        container_.push_back(value);
+        size_++;
     }
+
 
     void display()
     {
